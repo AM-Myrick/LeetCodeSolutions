c39a764727db919f29e1197db8d22043c0100f02
Andre Myrick <amyrick.820@gmail.com>
2019-09-30 14:33:18 -0400

    cleans up removeDuplicates solution

--- removeDuplicates/solution.py
+++ removeDuplicates/solution.py
@@ -7,10 +7,10 @@ class Solution:
     if len(nums) == 0 or len(nums) == 1:
       return len(nums)
     # remove the index as long as it's equal to the next index
-    i = 0
+    i, j = 0, 1
     while i < len(nums) - 1:
-      if nums[i] == nums[i + 1]:
+      if nums[i] == nums[j]:
         nums.pop(i)
         continue
-      i += 1
-    return len(nums)
+      i, j = i + 1, j + 1
+    return j
