891d830ec1c39171814ad36e4c76880e5111f0b7
Andre Myrick <amyrick.820@gmail.com>
2019-10-01 15:35:04 -0400

    adds comment to removeElement solution

--- removeElement/solution.py
+++ removeElement/solution.py
@@ -2,6 +2,7 @@
 from typing import List
 class Solution:
   def removeElement(self, nums: List[int], val: int) -> int:
+    # while val exists in nums it will be removed, once it doesn't exist nums length is returned
     while True:
       try:
         nums.remove(val)
