d783d03eb26472fa2ce3cf67a2e8a04376a919b2
Andre Myrick <amyrick.820@gmail.com>
2019-10-01 15:33:57 -0400

    adds comments to searchInsertPosition solution

--- searchInsertPosition/solution.py
+++ searchInsertPosition/solution.py
@@ -2,10 +2,12 @@
 from typing import List
 class Solution:
   def searchInsert(self, nums: List[int], target: int) -> int:
+    # check if target exists in list or if zeroth element is > target
     if target in nums:
       return nums.index(target)
     if nums[0] > target:
       return 0
+    # finds where target should be inserted
     for idx, num in enumerate(nums):
       if idx == len(nums) - 1 or num < target and nums[idx + 1] > target:
         return idx + 1
