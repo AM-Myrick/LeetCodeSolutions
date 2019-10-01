b4168d253261a35c4fa7657a59ca19902ea6c8da
Andre Myrick <amyrick.820@gmail.com>
2019-10-01 15:59:11 -0400

    makes removeElement solution simpler

--- removeElement/solution.py
+++ removeElement/solution.py
@@ -1,10 +1,9 @@
 # https://leetcode.com/problems/remove-element/
 from typing import List
+
 class Solution:
   def removeElement(self, nums: List[int], val: int) -> int:
     # while val exists in nums it will be removed, once it doesn't exist nums length is returned
-    while True:
-      try:
-        nums.remove(val)
-      except:
-        return len(nums)
+    while val in nums:
+      nums.remove(val)
+    return len(nums)
