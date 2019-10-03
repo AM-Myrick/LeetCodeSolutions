aab08d3d07666e7914652388b1ed2fa34d9868a1
Andre Myrick <amyrick.820@gmail.com>
2019-10-03 08:52:55 -0400

    adds Sort Characters by Frequency solution

---
+++ Medium Questions/Sort Characters by Frequency/solution.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/sort-characters-by-frequency/
+from collections import Counter
+class Solution:
+  def frequencySort(self, s: str) -> str:
+    # counts number of times each element appears in s
+    count = Counter(s)
+    # adds every letter to a list the number of times it appears in s
+    # by order of frequency, then converts list to str
+    return "".join([letter[0] * letter[1] for letter in count.most_common()])
