965821bc12025f3f525ea4d53e868d71c9478422
Andre Myrick <amyrick.820@gmail.com>
2019-10-03 08:58:49 -0400

    improves var names in Sort Characters by Frequency

--- Medium Questions/Sort Characters by Frequency/solution.py
+++ Medium Questions/Sort Characters by Frequency/solution.py
@@ -6,4 +6,4 @@ class Solution:
     count = Counter(s)
     # adds every letter to a list the number of times it appears in s
     # by order of frequency, then converts list to str
-    return "".join([letter[0] * letter[1] for letter in count.most_common()])
+    return "".join([letter * frequency for letter, frequency in count.most_common()])
