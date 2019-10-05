55e0ae12fb5ebaae2fd308cc663dda04583e96aa
Andre Myrick <amyrick.820@gmail.com>
2019-10-05 18:36:35 -0400

    updates Intersection of Two Arrays II solution

--- Easy Questions/Intersection of Two Arrays II/solution.py
+++ Easy Questions/Intersection of Two Arrays II/solution.py
@@ -3,16 +3,22 @@ from typing import List
 from collections import Counter
 class Solution:
   def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+    # create a function to ensure the array with less elements is iterated over
+    def find_intersects(first_nums, second_nums):
+      result = []
+      for key, value in first_nums.items():
+        if key in second_nums and value <= second_nums[key]:
+          while value:
+            result.append(key)
+            value -= 1
+        elif key in second_nums and value > second_nums[key]:
+          while second_nums[key]:
+            result.append(key)
+            second_nums[key] -= 1
+      return result
+    
     nums1_counter = Counter(nums1)
     nums2_counter = Counter(nums2)
-    result: List = []
-    for key, value in nums1_counter.items():
-      if key in nums2_counter and value <= nums2_counter[key]:
-        while value:
-          result.append(key)
-          value -= 1
-      elif key in nums2_counter and value > nums2_counter[key]:
-        while nums2_counter[key]:
-          result.append(key)
-          nums2_counter[key] -= 1
-    return result
+    return find_intersects(nums1_counter, nums2_counter) if len(nums1_counter) <=                     len(nums2_counter) else find_intersects(nums2_counter, nums1_counter)
+    
+    
