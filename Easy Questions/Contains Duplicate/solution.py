ad0baf1eb5d8724ea9ee725dbfa755c54118e452
AM-Myrick <amyrick.820@gmail.com>
2019-10-11 22:56:21 -0400

    updates Contains Duplicate Python solution to be worst case O(n)

--- Easy Questions/Contains Duplicate/solution.py
+++ Easy Questions/Contains Duplicate/solution.py
@@ -1,8 +1,12 @@
 from typing import List, Set
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        # set data structure removes duplicates
-        # then return whether or not the length of the set
-        # and the original list are the same
-        copy: Set = set(nums)
-        return len(copy) != len(nums)
+        # create a dict and add the numbers
+        # return true if a number has already been added
+        # return false if the loop over the list completes
+        copy: Set = set()
+        for num in nums:
+            if num in copy:
+                return True
+            copy.add(num)
+        return False
