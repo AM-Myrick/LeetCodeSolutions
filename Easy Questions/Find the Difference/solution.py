54b9522bee0ddd796c7aaed2ac7d446f2767ec38
AM-Myrick <amyrick.820@gmail.com>
2019-10-12 18:18:19 -0400

    adds Find the Difference solution

---
+++ Easy Questions/Find the Difference/solution.py
@@ -0,0 +1,11 @@
+from collections import Counter
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        # converts s & t to a dict {letter: number of times it appears}
+        s_counter, t_counter = Counter(s), Counter(t)
+        
+        # iterates over the keys and checks if a letter isn't in s
+        # or if it appears in t more often than in s
+        for letter in t_counter.keys():
+            if letter not in s_counter or s_counter[letter] != t_counter[letter]:
+                return letter
