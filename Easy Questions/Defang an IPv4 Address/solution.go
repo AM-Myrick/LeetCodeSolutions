d76eb5ba20cb492f8fbcdc83da68afa67704130c
AM-Myrick <amyrick.820@gmail.com>
2019-10-19 11:22:21 -0400

    adds Defang an IPv4 Address js solution

--- Easy Questions/Defang an IPv4 Address/solution.go
+++ Easy Questions/Defang an IPv4 Address/solution.go
@@ -1,6 +1,19 @@
-// https://leetcode.com/problems/defanging-an-ip-address/
+// Package defangipaddr https://leetcode.com/problems/defanging-an-ip-address/
+// Given a valid (IPv4) IP address, return a defanged version of that IP address.
+// A defanged IP address replaces every period "." with "[.]".
 
-package main
+//  Example 1:
+// Input: address = "1.1.1.1"
+// Output: "1[.]1[.]1[.]1"
+
+// Example 2:
+// Input: address = "255.100.50.0"
+// Output: "255[.]100[.]50[.]0"
+
+// Constraints:
+// The given address is a valid IPv4 address.
+
+package defangipaddrs
 
 func defangIPaddr(address string) string {
 	copy := ""
