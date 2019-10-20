bf4b8ee8ac950fda56db83b4da6b3da829976c2d
AM-Myrick <amyrick.820@gmail.com>
2019-10-19 20:26:12 -0400

    adds js Implement Square Root solution

--- Easy Questions/Implement Square Root/solution.go
+++ Easy Questions/Implement Square Root/solution.go
@@ -1,7 +1,28 @@
-// https://leetcode.com/problems/sqrtx/submissions/
-package main
+// Package mysqrt - https://leetcode.com/problems/sqrtx/
+// Implement int sqrt(int x).
+
+// Compute and return the square root of x,
+// where x is guaranteed to be a non-negative integer.
+
+// Since the return type is an integer,
+// the decimal digits are truncated and only the integer part of the result is returned.
+
+// Example 1:
+
+// Input: 4
+// Output: 2
+// Example 2:
+
+// Input: 8
+// Output: 2
+// Explanation: The square root of 8 is 2.82842..., and since
+// the decimal part is truncated, 2 is returned.
+
+package mysqrt
+
 import "math"
+
 func mySqrt(x int) int {
-    // math.Pow only accepts valid float64 types
-    return int(math.Pow(float64(x), 0.5))
+	// math.Pow only accepts valid float64 types
+	return int(math.Pow(float64(x), 0.5))
 }
